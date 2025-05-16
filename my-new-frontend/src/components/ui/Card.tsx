c831256ff5fe2418e51714cc8719ad6dda60b335
VijaydityaSarker <your_email@example.com>
2025-05-16 21:53:31 +0100

    Unwanted files deleted

--- my-new-frontend/src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-interface CardProps {
-    children: ReactNode;
-}
-
-export function Card({ children }: CardProps) {
-    return <div className="glass-card">{children}</div>;
-}
